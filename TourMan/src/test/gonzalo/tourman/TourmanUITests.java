7c903534325d1a13e9f2b53e9d849da2aa9f8520
gborobio73 <gborobio@gmail.com>
2013-03-18 14:16:08 +0200

    added tourman ui tests

--- TourMan/src/test/gonzalo/tourman/TourmanUITests.java
+++ TourMan/src/test/gonzalo/tourman/TourmanUITests.java
@@ -1,14 +1,34 @@
 package test.gonzalo.tourman;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.gonzalo.tourman.UI.IMainUIBuilder;
+import com.gonzalo.tourman.UI.TourmanUI;
+import com.gonzalo.tourman.UI.Components.MainUIBuilder;
+import com.vaadin.ui.UI;
 
 public class TourmanUITests {
 
+	TourmanUI sut ;
+	IMainUIBuilder mainUIBuilder;
+	
+	@Before
+    public void setUp() {
+		UI ui = mock(UI.class);
+		mainUIBuilder = mock(IMainUIBuilder.class);
+        sut = new TourmanUI(ui, mainUIBuilder);
+    }
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		sut.Build();
+		
+		verify(mainUIBuilder).createNavigationMenu();
+		verify(mainUIBuilder).createWelcome();
 	}
 
 }
