6e904afe00a22c08595c1d2d6fe4a0a44fe88c58
gborobio73 <gborobio@gmail.com>
2013-03-18 13:29:57 +0200

    added test folders

---
+++ TourMan/src/test/gonzalo/tourman/UI/TourmanUITests.java
@@ -0,0 +1,14 @@
+package test.gonzalo.tourman.UI;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TourmanUITests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
